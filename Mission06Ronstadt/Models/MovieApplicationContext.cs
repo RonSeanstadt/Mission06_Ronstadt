ae5ae70f2e07d02384874695484c6e683e0c849c
RonSeanstadt <sronstadt470@gmail.com>
2024-02-19 09:41:04 -0700

    Added new branch for Mission 7 Assignment

--- Mission06Ronstadt/Models/MovieApplicationContext.cs
+++ Mission06Ronstadt/Models/MovieApplicationContext.cs
@@ -2,7 +2,7 @@
 
 namespace Mission06Ronstadt.Models
 {
-    public class MovieApplicationContext : DbContext
+    public class MovieApplicationContext : DbContext // Liaison from the app to the database
     {
         public MovieApplicationContext(DbContextOptions<MovieApplicationContext> options) : base (options) // Constructor
         {
