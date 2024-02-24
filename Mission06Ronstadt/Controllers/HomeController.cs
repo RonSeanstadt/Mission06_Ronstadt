42969833394863e35630c6213cbbed37251a9e78
RonSeanstadt <sronstadt470@gmail.com>
2024-02-23 20:42:37 -0700

    Added all the functionality in deleting and updating tables, but had an error I couldn't figure out with the Collection view not loading... Also, when I filled out the movie submission form, it always expected the Application model, and I didn't know how to make it recieve the new Movie model, and I ran out of time before the submission deadline for the assignment. That sums up the issues I had.

--- Mission06Ronstadt/Controllers/HomeController.cs
+++ Mission06Ronstadt/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Mission06Ronstadt.Models;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Mission06Ronstadt.Controllers
 {
@@ -21,18 +22,83 @@ namespace Mission06Ronstadt.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Share()
         {
-            return View();
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.Category)
+                .ToList();
+
+            return View("Share", new Movie());
         }
 
         [HttpPost]
-        public IActionResult Share(Application response)
+        public IActionResult Share(Movie response)
         {
-            _context.Applications.Add(response); // Adds record to the database
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response); // Adds record to the database
+                _context.SaveChanges();
+
+                return View("Submitted", response);
+            }
+            else // Invalid Data
+            {
+                ViewBag.Categories = _context.Categories
+                    .OrderBy(x => x.Category)
+                    .ToList();
+
+                return View(response);
+            }
+        }
+
+        public IActionResult Collection()
+        {
+            // Linq
+            var movies = _context.Movies
+                .OrderBy(x => x.Title).ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            ViewBag.Categories = _context.Categories
+                .OrderBy(x => x.Category)
+                .ToList();
+
+            return View("Share", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie updatedInfo)
+        {
+            _context.Update(updatedInfo);
             _context.SaveChanges();
 
-            return View("Submitted", response);
+            return RedirectToAction("Share");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("Collection");
         }
     }
 }
---
+++ Mission06Ronstadt/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission06Ronstadt.Models
+{
+    public class Movie
+    {
+        [Key]
+        [Required]
+        public int MovieId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public int? CategoryId { get; set; }
+        public Categories? Category { get; set; }
+
+        [Required(ErrorMessage = "You need to enter a movie title")]
+        public string Title { get; set; }
+
+        [Range(1888, 2024)]
+        [Required(ErrorMessage = "You need to enter a valid year in which the movie was released")]
+        public int Year { get; set; }
+
+        public string? Director { get; set; }
+
+        public string? Rating { get; set; }
+        public bool Edited { get; set; }
+        public string? LentTo { get; set; }
+        public bool CopiedToPlex { get; set;}
+
+        [MaxLength(25)]
+        public string? Notes { get; set; }
+    }
+}
